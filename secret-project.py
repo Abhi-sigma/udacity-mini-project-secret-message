45461cd75ea97a3d2404a4253646a2b22bccdfab
Abhi-sigma <nonestate@gmail.com>
2017-07-06 16:07:37 +1000

    added comments

--- secret-project.py
+++ secret-project.py
@@ -3,9 +3,13 @@ import string
 import shutil
 import time
 
+# insert the message in secret message
 secret_message="ineedsleep"
+#make a empty dictionary to add mapping of alphabets to pictures in folder
 mapping={}
 index_count=0
+
+#the path where you keep folder
 path=r"c:/users/nones/desktop/alphabet"
 folder_name="alphabet"
 original_folder=os.path.join(path,folder_name)
@@ -22,6 +26,7 @@ for items in  file_name_list:
 		index_count+=1
 		# print mapping
 	except:
+# 		for now
 		pass 
 print mapping
 
@@ -42,7 +47,9 @@ else:
 r_counter=0
 for items in secret_message:
 	file_needed=mapping[items]
-	print file_needed
+# 	print file_needed
+# copy from original folder to the secret folder,all the files that is needed to encode message
 	shutil. copyfile(os.path.join(original_folder,file_needed),os.path.join(secret_folder,file_needed))
+# 	need to rename because files are sorted in alphabetical order in window,no idea about other os
 	os.rename(os.path.join(secret_folder,file_needed),os.path.join(secret_folder,str(r_counter)+file_needed))
 	r_counter+=1
